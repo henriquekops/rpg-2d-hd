1a50afdb2de2d70012f05204c6a6d9f439aed83c
henrique.kops <henrique.kops@gmail.com>
2024-02-04 03:42:16 -0300

    almost there, version 0.1!

--- scripts/splash_screen.gd
+++ scripts/splash_screen.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "do_splash":
-		get_tree().change_scene_to_file("res://scenes/main.tscn")
+		get_tree().change_scene_to_file("res://scenes/home.tscn")
